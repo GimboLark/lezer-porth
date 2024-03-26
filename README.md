1dd7adfcb43df199b8ee9068634963a12b981a04
gimbolark <gimbolark@hotmail.com>
2024-03-27 00:34:25 +0300

    fixed bug

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # lezer-porth
-very minimal porth grammar for lezer
+very very minimal porth grammar for lezer
 it doesnt cover most of porths grammar
+this is a test
+it is to use it in motion-canvas
+
+![example from motion-canvas](image.png)
--- src/highlight.js
+++ src/highlight.js
@@ -1,11 +1,9 @@
 import { styleTags, tags } from "@lezer/highlight"
 
 export const highlighting = styleTags({
-    Identifier: tags.name,
-    Number:     tags.name,
-    String:     tags.name,
-    Intrinsics: tags.name,
-    Keywords:   tags.name,
-    Control:    tags.name,
-    Paranteces: tags.name
+    Number:     tags.number,
+    String:     tags.string,
+    Intrinsics: tags.literal,
+    Keywords:   tags.keyword,
+    Control:    tags.content
 })
